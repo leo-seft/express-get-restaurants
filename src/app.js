8ebb48952b4d2e3191b7980deb0ebacc58fb9ff5
leo-seft <leosefton13@hotmail.com>
2024-11-04 16:19:01 +0000

    adding parts 1,2 and 3

--- src/app.js
+++ src/app.js
@@ -2,10 +2,41 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
+const res = require("express/lib/response");
+const req = require("express/lib/request");
 
 //TODO: Create your GET Request Route Below: 
 
+app.get("/restaurants", async (request, response) => {
+    const restaurants = await Restaurant.findAll();
+    response.json(restaurants);
+});
 
+app.get("/restaurants/:id", async (request, response) => {
+    const restaurantId = request.params.id;
+    const restaurant = await Restaurant.findByPk(restaurantId);
+    response.json(restaurant);
+})
 
+app.use(express.json())
+
+app.post("/restaurants", async (request,response) => {
+    const newRestaurant = await Restaurant.create(request.body);
+    response.json(newRestaurant)
+})
+
+app.put("/restaurants/:id", async(request, response) => {
+    const restaurantId = request.params.id;
+    let restaurant = await Restaurant.findByPk(restaurantId);
+    restaurant = await restaurant.update(request.body)
+    response.json(restaurant)
+})
+
+app.delete("/restaurants/:id", async(request, response) => {
+    const restaurantId = request.params.id;
+    let restaurant = await Restaurant.findByPk(restaurantId);
+    restaurant = await restaurant.destroy()
+    response.status(204).send()
+})
 
 module.exports = app;
